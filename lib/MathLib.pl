e690c044039b9a23c9c0947deb6892ef73e6f2c0
Mike Miller <emkey1@gmail.com>
2025-08-11 19:22:04 -0700

    Add math utility unit

---
+++ lib/MathLib.pl
@@ -0,0 +1,134 @@
+unit MathLib;
+
+interface
+
+function ArcTan(x: real): real;
+function ArcSin(x: real): real;
+function ArcCos(x: real): real;
+function Cotan(x: real): real;
+function Power(base, exponent: real): real;
+function Log10(x: real): real;
+function Sinh(x: real): real;
+function Cosh(x: real): real;
+function Tanh(x: real): real;
+function Max(a, b: real): real;
+function Min(a, b: real): real;
+function Floor(x: real): integer;
+function Ceil(x: real): integer;
+
+implementation
+
+const
+  PI = 3.141592653589793;
+  LN10 = 2.302585092994046;
+
+function ArcTan(x: real): real;
+var
+  term, sum, xpow: real;
+  n: integer;
+begin
+  if x > 1.0 then
+    ArcTan := PI/2 - ArcTan(1.0 / x)
+  else if x < -1.0 then
+    ArcTan := -PI/2 - ArcTan(1.0 / x)
+  else
+  begin
+    term := x;
+    sum := x;
+    xpow := x;
+    n := 1;
+    repeat
+      xpow := -xpow * x * x;
+      n := n + 2;
+      term := xpow / n;
+      sum := sum + term;
+    until abs(term) < 1e-10;
+    ArcTan := sum;
+  end;
+end;
+
+function ArcSin(x: real): real;
+begin
+  if x >= 1.0 then
+    ArcSin := PI / 2
+  else if x <= -1.0 then
+    ArcSin := -PI / 2
+  else
+    ArcSin := ArcTan(x / sqrt(1.0 - x * x));
+end;
+
+function ArcCos(x: real): real;
+begin
+  ArcCos := PI / 2 - ArcSin(x);
+end;
+
+function Cotan(x: real): real;
+begin
+  Cotan := 1.0 / tan(x);
+end;
+
+function Power(base, exponent: real): real;
+begin
+  Power := exp(exponent * ln(base));
+end;
+
+function Log10(x: real): real;
+begin
+  Log10 := ln(x) / LN10;
+end;
+
+function Sinh(x: real): real;
+begin
+  Sinh := (exp(x) - exp(-x)) / 2.0;
+end;
+
+function Cosh(x: real): real;
+begin
+  Cosh := (exp(x) + exp(-x)) / 2.0;
+end;
+
+function Tanh(x: real): real;
+begin
+  Tanh := Sinh(x) / Cosh(x);
+end;
+
+function Max(a, b: real): real;
+begin
+  if a > b then
+    Max := a
+  else
+    Max := b;
+end;
+
+function Min(a, b: real): real;
+begin
+  if a < b then
+    Min := a
+  else
+    Min := b;
+end;
+
+function Floor(x: real): integer;
+var
+  t: integer;
+begin
+  t := trunc(x);
+  if (x < 0.0) and (x <> t) then
+    Floor := t - 1
+  else
+    Floor := t;
+end;
+
+function Ceil(x: real): integer;
+var
+  t: integer;
+begin
+  t := trunc(x);
+  if (x > 0.0) and (x <> t) then
+    Ceil := t + 1
+  else
+    Ceil := t;
+end;
+
+end.
+
