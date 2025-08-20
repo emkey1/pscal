e592c151eae6d5967aa06b0a46728d7a8caf7f54
Mike Miller <emkey1@gmail.com>
2025-08-20 08:04:24 -0700

    Extend SDL graphics test for CLike

--- Tests/clike/graphics.c
+++ Tests/clike/graphics.c
@@ -4,6 +4,15 @@ int main() {
     initgraph(640, 480, "clike graphics test");
     w = getmaxx();
     h = getmaxy();
+
+    cleardevice();
+    setcolor(15);
+    putpixel(5, 5);
+    drawline(0, 0, w, h);
+    drawrect(w / 4, h / 4, w * 3 / 4, h * 3 / 4);
+    updatescreen();
+    graphloop(10);
+
     printf(w);
     printf(" ");
     printf(h);
