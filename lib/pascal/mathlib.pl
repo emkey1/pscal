483d2cf4ac7dd16603860e803599254e51e1a777
Mike Miller <emkey1@gmail.com>
2025-09-03 06:20:15 -0700

    Update mathlib.pl
    Forgot some constants.  Fixed.

--- lib/pascal/mathlib.pl
+++ lib/pascal/mathlib.pl
@@ -5,6 +5,15 @@ interface
 { Thin wrappers over VM builtins. Kept for compatibility with existing code
   that `uses MathLib;` so projects need not be updated. }
 
+const
+  { Common mathematical constants }
+  Pi      = 3.1415926535897932384626433832795;
+  E       = 2.7182818284590452353602874713527;
+  Ln2     = 0.69314718055994530941723212145818;
+  Ln10    = 2.3025850929940456840179914546844;
+  TwoPi   = 2.0 * Pi;
+  PiOver2 = Pi / 2.0;
+
 function ArcTan(x: real): real;
 function ArcSin(x: real): real;
 function ArcCos(x: real): real;
