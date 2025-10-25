69a71cea0fe4fc95d0e4c10676e48eb172f3d942
Mike Miller <emkey1@gmail.com>
2025-10-25 20:02:12 +0100

    Stabilize shell bytecode caching for temporary scripts

--- src/shell/function.h
+++ src/shell/function.h
@@ -1,9 +1,13 @@
 #ifndef SHELL_FUNCTION_H
 #define SHELL_FUNCTION_H
 
+#include <stdint.h>
 #include "compiler/bytecode.h"
 
+#define SHELL_COMPILED_FUNCTION_MAGIC 0x5343464eU /* 'SCFN' */
+
 typedef struct ShellCompiledFunction {
+    uint32_t magic;
     BytecodeChunk chunk;
 } ShellCompiledFunction;
 
