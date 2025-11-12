cbaaea4bb40da6982c04c79307071814abeb7b3f
Mike Miller <emkey1@oneopa.com>
2025-11-12 07:27:42 -0800

    ios: guard AttributedString trimming

--- ios/Sources/App/AttributedString+Trim.swift
+++ ios/Sources/App/AttributedString+Trim.swift
@@ -2,10 +2,17 @@ import Foundation
 
 extension AttributedString {
     func trimmedTrailingSpaces() -> AttributedString {
-        var copy = self
-        while copy.characters.last == " " {
-            copy.characters.removeLast()
+        var trimmed = self
+        var index = trimmed.endIndex
+        while index > trimmed.startIndex {
+            let previous = trimmed.index(before: index)
+            if trimmed[previous] == " " {
+                trimmed.removeSubrange(previous..<index)
+                index = previous
+            } else {
+                break
+            }
         }
-        return copy
+        return trimmed
     }
 }
