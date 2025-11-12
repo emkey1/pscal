c79c91f575da1800c2b51af5f3d55130f884ffdc
Mike Miller <emkey1@oneopa.com>
2025-11-12 08:12:52 -0800

    ios: trim attributed strings via character array

--- ios/Sources/App/AttributedString+Trim.swift
+++ ios/Sources/App/AttributedString+Trim.swift
@@ -3,16 +3,10 @@ import Foundation
 extension AttributedString {
     func trimmedTrailingSpaces() -> AttributedString {
         var trimmed = self
-        var index = trimmed.endIndex
-        while index > trimmed.startIndex {
-            let prev = trimmed.index(before: index)
-            if trimmed[prev] == " " {
-                trimmed.removeSubrange(prev..<index)
-                index = prev
-            } else {
-                break
-            }
+        var characters = Array(trimmed.characters)
+        while let last = characters.last, last == " " {
+            characters.removeLast()
         }
-        return trimmed
+        return AttributedString(characters)
     }
 }
