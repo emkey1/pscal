2bf52909349f9c2175ca19dfe4f89826f5195302
Mike Miller <emkey1@oneopa.com>
2025-11-12 07:35:29 -0800

    ios: use removeLast for trimming

--- ios/Sources/App/AttributedString+Trim.swift
+++ ios/Sources/App/AttributedString+Trim.swift
@@ -3,15 +3,8 @@ import Foundation
 extension AttributedString {
     func trimmedTrailingSpaces() -> AttributedString {
         var trimmed = self
-        while trimmed.endIndex > trimmed.startIndex {
-            let before = trimmed.index(before: trimmed.endIndex)
-            let range = before..<trimmed.endIndex
-            let segment = trimmed[range]
-            if String(segment.characters) == " " {
-                trimmed.removeSubrange(range)
-            } else {
-                break
-            }
+        while let last = trimmed.last, last == " " {
+            trimmed.removeLast()
         }
         return trimmed
     }
