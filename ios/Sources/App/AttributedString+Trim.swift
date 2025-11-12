417fcd90d75aefcaa637dfba63729af9734c049f
Mike Miller <emkey1@oneopa.com>
2025-11-12 07:37:25 -0800

    ios: fix AttributedString trimming API usage

--- ios/Sources/App/AttributedString+Trim.swift
+++ ios/Sources/App/AttributedString+Trim.swift
@@ -3,8 +3,15 @@ import Foundation
 extension AttributedString {
     func trimmedTrailingSpaces() -> AttributedString {
         var trimmed = self
-        while let last = trimmed.last, last == " " {
-            trimmed.removeLast()
+        var index = trimmed.endIndex
+        while index > trimmed.startIndex {
+            let prev = trimmed.index(before: index)
+            if trimmed[prev] == " " {
+                trimmed.removeSubrange(prev..<index)
+                index = prev
+            } else {
+                break
+            }
         }
         return trimmed
     }
