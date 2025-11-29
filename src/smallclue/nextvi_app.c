24b6dd441a7ecafd05c85107a7b9e347fe43de4c
Mike Miller <emkey1@oneopa.com>
2025-11-28 16:42:02 -0800

    Use pty fallback for nextvi on iOS when /dev/tty unavailable

--- src/smallclue/nextvi_app.c
+++ src/smallclue/nextvi_app.c
@@ -38,6 +38,41 @@ static void smallclueRestoreEnv(const char *name, char *saved) {
     }
 }
 
+#if defined(PSCAL_TARGET_IOS)
+static int smallclueOpenPty(void) {
+    int master = posix_openpt(O_RDWR | O_NOCTTY);
+    if (master < 0) {
+        return -1;
+    }
+    if (grantpt(master) != 0 || unlockpt(master) != 0) {
+        close(master);
+        return -1;
+    }
+    char slave_name[128];
+    if (ptsname_r(master, slave_name, sizeof(slave_name)) != 0) {
+        close(master);
+        return -1;
+    }
+    int slave = open(slave_name, O_RDWR | O_NOCTTY);
+    if (slave < 0) {
+        close(master);
+        return -1;
+    }
+    if (dup2(slave, STDIN_FILENO) < 0 ||
+        dup2(slave, STDOUT_FILENO) < 0 ||
+        dup2(slave, STDERR_FILENO) < 0) {
+        close(slave);
+        close(master);
+        return -1;
+    }
+    if (slave != STDIN_FILENO && slave != STDOUT_FILENO && slave != STDERR_FILENO) {
+        close(slave);
+    }
+    close(master);
+    return STDIN_FILENO;
+}
+#endif
+
 static int smallclueSetupTty(void) {
     int fd = open("/dev/tty", O_RDWR);
     if (fd >= 0) {
@@ -48,6 +83,11 @@ static int smallclueSetupTty(void) {
             dup2(fd, STDOUT_FILENO);
         }
     }
+#if defined(PSCAL_TARGET_IOS)
+    if (fd < 0) {
+        fd = smallclueOpenPty();
+    }
+#endif
     return fd;
 }
 
