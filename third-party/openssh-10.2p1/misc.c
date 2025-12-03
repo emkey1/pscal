0a1ee4fa0d60a533adaf3bff652b6ca3e8197f2c
Mike Miller <emkey1@oneopa.com>
2025-12-03 09:47:23 -0800

    o Fixed merge related errors

--- third-party/openssh-10.2p1/misc.c
+++ third-party/openssh-10.2p1/misc.c
@@ -427,7 +427,7 @@ timeout_connect(int sockfd, const struct sockaddr *serv_addr,
 	int optval = 0;
 	socklen_t optlen = sizeof(optval);
 #ifdef PSCAL_TARGET_IOS
-	pscalRuntimeDebugLog("timeout_connect: start fd=%d timeout=%d", sockfd, timeoutp ? *timeoutp : -1);
+	pscal_debug_logf("timeout_connect: start fd=%d timeout=%d", sockfd, timeoutp ? *timeoutp : -1);
 	fprintf(stderr, "timeout_connect: start fd=%d timeout=%d\n", sockfd, timeoutp ? *timeoutp : -1);
 	__block volatile sig_atomic_t cancelled = 0;
 	dispatch_block_t watchdog = NULL;
@@ -453,7 +453,7 @@ timeout_connect(int sockfd, const struct sockaddr *serv_addr,
 			continue;
 		else if (errno != EINPROGRESS) {
 #ifdef PSCAL_TARGET_IOS
-			pscalRuntimeDebugLog("timeout_connect: connect failed errno=%d", errno);
+			pscal_debug_logf("timeout_connect: connect failed errno=%d", errno);
 			fprintf(stderr, "timeout_connect: connect failed errno=%d\n", errno);
 #endif
 			return -1;
@@ -486,7 +486,7 @@ timeout_connect(int sockfd, const struct sockaddr *serv_addr,
 #endif
 	    )) == -1) {
 #ifdef PSCAL_TARGET_IOS
-		pscalRuntimeDebugLog("timeout_connect: waitfd failed errno=%d", errno);
+		pscal_debug_logf("timeout_connect: waitfd failed errno=%d", errno);
 		fprintf(stderr, "timeout_connect: waitfd failed errno=%d\n", errno);
 		if (watchdog) {
 			dispatch_block_cancel(watchdog);
