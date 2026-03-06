e7f316e04a9b6dcf2c6da2081d11b91a03cac81a
Mike Miller <emkey1@oneopa.com>
2026-03-06 16:16:41 +0000

    ios: stabilize concurrent embedded micro stdio routing

--- ios/Tools/go_patches/tcell_tscreen_unix_darwin.go
+++ ios/Tools/go_patches/tcell_tscreen_unix_darwin.go
@@ -5,6 +5,9 @@ package tcell
 import (
 	"io"
 	"os"
+	"strconv"
+	"sync"
+	"syscall"
 
 	"github.com/zyedidia/poller"
 )
@@ -13,9 +16,83 @@ func pscalUseEmbeddedStdio() bool {
 	return true
 }
 
+func pscalParseStdioFD(name string, fallback int) int {
+	value := os.Getenv(name)
+	if value == "" {
+		return fallback
+	}
+	fd, err := strconv.Atoi(value)
+	if err != nil || fd < 0 {
+		return fallback
+	}
+	return fd
+}
+
+func pscalDupFD(fd int) (int, error) {
+	for {
+		dupFD, err := syscall.Dup(fd)
+		if err == nil {
+			return dupFD, nil
+		}
+		if err != syscall.EINTR {
+			return -1, err
+		}
+	}
+}
+
+var pscalStdioProviderMu sync.RWMutex
+var pscalStdioProvider func() (int, int, bool)
+
+func SetPSCALStdioProvider(provider func() (int, int, bool)) {
+	pscalStdioProviderMu.Lock()
+	pscalStdioProvider = provider
+	pscalStdioProviderMu.Unlock()
+}
+
+func pscalResolveStdioFDs() (int, int) {
+	pscalStdioProviderMu.RLock()
+	provider := pscalStdioProvider
+	pscalStdioProviderMu.RUnlock()
+	if provider != nil {
+		if inFD, outFD, ok := provider(); ok && inFD >= 0 && outFD >= 0 {
+			return inFD, outFD
+		}
+	}
+	inFD := pscalParseStdioFD("PSCALI_TCELL_STDIN_FD", int(os.Stdin.Fd()))
+	outFD := pscalParseStdioFD("PSCALI_TCELL_STDOUT_FD", int(os.Stdout.Fd()))
+	return inFD, outFD
+}
+
 func openTty() (io.Reader, io.Writer, error) {
 	_ = pscalUseEmbeddedStdio
-	return os.Stdin, os.Stdout, nil
+	inFD, outFD := pscalResolveStdioFDs()
+
+	dupIn, err := pscalDupFD(inFD)
+	if err != nil {
+		return nil, nil, err
+	}
+	dupOut, err := pscalDupFD(outFD)
+	if err != nil {
+		_ = syscall.Close(dupIn)
+		return nil, nil, err
+	}
+
+	in := os.NewFile(uintptr(dupIn), "/dev/tty")
+	out := os.NewFile(uintptr(dupOut), "/dev/tty")
+	if in == nil || out == nil {
+		if in != nil {
+			_ = in.Close()
+		} else {
+			_ = syscall.Close(dupIn)
+		}
+		if out != nil {
+			_ = out.Close()
+		} else {
+			_ = syscall.Close(dupOut)
+		}
+		return nil, nil, syscall.EBADF
+	}
+	return in, out, nil
 }
 
 func closeTty(f interface{}) {
